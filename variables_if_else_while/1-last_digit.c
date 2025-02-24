7f45b3fb55cdb33c0f3ae5a51401ebaf3f0bc75c
“MDDO0” <maado321@gmail.com>
2025-02-24 00:14:09 +0000

    a

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+/* more headers goas there 
+ *
+ * betty style doc for function main there */
+int main(void){
+	int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0) {
+	printf("%d is positive ")
+} else if (n = 0) { 
+	printf("%d is zero ")
+} else {
+	printf("%d is negative ")
+return (0);
+}
