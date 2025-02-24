e9206285f0fbf676831054addde465ce21a30873
“MDDO0” <maado321@gmail.com>
2025-02-24 16:30:55 +0000

    a

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Generates program that prints the alphabet in lowercase
+ * , followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	for (char a = 'a' ; a <= 122 ; a++)
+	{
+	putchar(a);
+}	   
+        return (0);
+}
