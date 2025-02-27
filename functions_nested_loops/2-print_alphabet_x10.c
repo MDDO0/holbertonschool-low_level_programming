84b86a00562237562b546d1cb59e5be5b8a3a0c5
“MDDO0” <maado321@gmail.com>
2025-02-27 17:16:57 +0000

    a

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Description: This function prints times 10 times all lowercase letters
+ * from 'a' to 'z' using `_putchar` and adds a new line at the end.
+ *
+ * Return: void (no return value)
+ */
+void print_alphabet_x10(void)
+{
+int b = 1;
+for (; b <= 10; b++)
+{
+char a = 'a';
+for (; a <= 'z' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}}
