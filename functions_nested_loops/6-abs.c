1614cd8fb2e1807df83c6365f07ae3b5bd3efc62
“MDDO0” <maado321@gmail.com>
2025-02-28 00:03:19 +0000

    a

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include"main.h"
 /**
-*_abs-Computestheabsolutevalueofaninteger.
-*@n:Theintegerinput
-*Return:Theabsolutevalueofn.
+* _abs - Computes the absolute value of an integer.
+* @n: The integer input
+* Return: The absolute value of n.
 */
 int _abs(int n)
 {
-if(n<0)
-return(-n);
-return(n);
+if (n < 0)
+return (-n);
+return (n);
 }
