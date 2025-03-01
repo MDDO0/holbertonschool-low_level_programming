c470b334d5f709bad7d9f0b5a279c6ce5e3145d2
“MDDO0” <maado321@gmail.com>
2025-03-01 16:52:30 +0000

    a

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value of an int to 98
+ * @n: Pointer to the integer to update
+ *
+ * Description: This function takes a pointer to an int
+ * and modifies the value stored in that address to 98.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
