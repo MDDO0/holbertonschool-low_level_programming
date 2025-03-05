9b9695674ad0ff9c3e365037ad7defbd299dbf99
“MDDO0” <maado321@gmail.com>
2025-03-05 16:32:02 +0000

    a

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program's name
+ * @argc: Argument count (unused)
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
