81e6a33527e1c062e187e36165e982ce7409e423
“MDDO0” <maado321@gmail.com>
2025-03-15 19:21:36 +0000

    a

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog structure
+ * @name: pointer to the dog's name
+ * @age: dog's age in float
+ * @owner: pointer to the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
