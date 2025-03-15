ad87774b039e0845e89b783620bc06beec4b6657
“MDDO0” <maado321@gmail.com>
2025-03-15 19:38:07 +0000

    a

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,22 @@
 #define DOG_H
 
 /**
- * struct dog - defines a dog structure
- * @name: pointer to the dog's name
- * @age: dog's age in float
- * @owner: pointer to the dog's owner
+ * struct dog - dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
-#endif
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
